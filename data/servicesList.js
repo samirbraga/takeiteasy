e92fe607c13c7b471a656e416ce7d97df482b034
Samir Braga <Samir Braga>
2017-01-24 12:08:23 -0300

    Seventh Update - Minified

--- data/servicesList.js
+++ data/servicesList.js
@@ -1,20 +1,20 @@
 let services = [
-	{name: "Illustrações", imageSrc: 'Illustrações.png'},
-	{name: "Estampas", imageSrc: 'estampa.png'},
-	{name: "Edição de Vídeo", imageSrc: 'Audiovisual.png'},
-	{name: "Criação de Logos", imageSrc: 'logo.png'},
-	{name: "Correção de Redações", imageSrc: 'Redação.png'},
-	{name: "Confecção de Plantas", imageSrc: 'Plantas.png'},
-	{name: "Edição de Fotos", imageSrc: 'Edição-de-foto.png'},
-	{name: "Formatação e Correção de Trabalhos", imageSrc: 'Edição-de-foto.png'},
-	{name: "Banners", imageSrc: 'Edição-de-foto.png'},
-	{name: "Planilhas Automatizadas", imageSrc: 'Edição-de-foto.png'},
-	{name: "Convites em Geral", imageSrc: 'Edição-de-foto.png'},
-	{name: "Soluções em Softwares", imageSrc: 'Edição-de-foto.png'},
-	{name: "Planfletos", imageSrc: 'Edição-de-foto.png'},
-	{name: "Criação de Sites", imageSrc: 'Edição-de-foto.png'},
-	{name: "Slides", imageSrc: 'Edição-de-foto.png'},
-	{name: "Outdoors e Busdoors", imageSrc: 'Edição-de-foto.png'}
+	{name: "Illustrações", imageSrc: 'Illustrações.png', area: 'design'},
+	{name: "Estampas", imageSrc: 'estampa.png', area: 'design'},
+	{name: "Edição de Vídeo", imageSrc: 'Audiovisual.png', area: 'design'},
+	{name: "Criação de Logos", imageSrc: 'logo.png', area: 'design'},
+	{name: "Correção de Redações", imageSrc: 'Redação.png', area: 'academy'},
+	{name: "Confecção de Plantas", imageSrc: 'Plantas.png', area: 'design'},
+	{name: "Edição de Fotos", imageSrc: 'Edição-de-foto.png', area: 'design'},
+	{name: "Formatação e Correção de Trabalhos", imageSrc: 'Edição-de-foto.png', area: 'academy'},
+	{name: "Banners", imageSrc: 'Edição-de-foto.png', area: 'design'},
+	{name: "Planilhas Automatizadas", imageSrc: 'Edição-de-foto.png', area: 'software'},
+	{name: "Convites em Geral", imageSrc: 'Edição-de-foto.png', area: 'design'},
+	{name: "Soluções em Softwares", imageSrc: 'Edição-de-foto.png', area: 'software'},
+	{name: "Planfletos", imageSrc: 'Edição-de-foto.png', area: 'design'},
+	{name: "Criação de Sites", imageSrc: 'Edição-de-foto.png', area: 'design'},
+	{name: "Slides", imageSrc: 'Edição-de-foto.png', area: 'academy'},
+	{name: "Outdoors e Busdoors", imageSrc: 'Edição-de-foto.png', area: 'design'}
 ];
 
 function parseToUrl(string){
